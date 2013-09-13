d361ca9821927335c55e445bf1ebb5210f4d26bf
viki <viki@ROS.(none)>
2013-09-13 17:05:56 -0500

    Committing prior to fixing repo (removing files that should be untracked)

---
+++ hoverboard/msg_gen/lisp/_package.lisp
@@ -0,0 +1,19 @@
+(cl:defpackage hoverboard-msg
+  (:use )
+  (:export
+   "<SERVORAW>"
+   "SERVORAW"
+   "<GPIORAW>"
+   "GPIORAW"
+   "<ADCRAW>"
+   "ADCRAW"
+   "<PWMRAW>"
+   "PWMRAW"
+   "<GYRORAW>"
+   "GYRORAW"
+   "<THRUSTERRAW>"
+   "THRUSTERRAW"
+   "<LEDRAW>"
+   "LEDRAW"
+  ))
+
