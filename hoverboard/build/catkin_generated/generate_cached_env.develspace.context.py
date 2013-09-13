78956d171bc296f289d31e98870874a372d99586
viki <viki@ROS.(none)>
2013-09-13 17:11:20 -0500

    More build files

--- hoverboard/build/catkin_generated/generate_cached_env.develspace.context.py
+++
@@ -1,8 +0,0 @@
-catkin_EXTRAS_DIR="/opt/ros/groovy/share/catkin/cmake"
-
-ENV_SCRIPT="/home/viki/RosWorkspace/Project/hoverboard/build/devel/env.sh"
-OUTPUT_SCRIPT="/home/viki/RosWorkspace/Project/hoverboard/build/catkin_generated/env_cached.sh"
-
-PREPEND_SPACE_DIR="/home/viki/RosWorkspace/Project/hoverboard/build/devel"
-CMAKE_PREFIX_PATH="/home/viki/RosWorkspace/Project/hoverboard/build/devel;/home/viki/catkin_ws/devel;/opt/ros/groovy".split(';') if "/home/viki/RosWorkspace/Project/hoverboard/build/devel;/home/viki/catkin_ws/devel;/opt/ros/groovy" != "" else []
-PYTHON_INSTALL_DIR="lib/python2.7/dist-packages"
--- hoverboard/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/viki/RosWorkspace/Project/hoverboard/build/devel/setup.sh"
