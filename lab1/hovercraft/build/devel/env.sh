33c7fa81bf046f4c321cc7f54a66393748e18cb8
viki <viki@ROS.(none)>
2013-09-13 15:43:44 -0500

    Added hoverboard and hovercraft files (should ignore the build files later...)

---
+++ lab1/hovercraft/build/devel/env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/cmake/templates/env.sh.in
+
+if [ $# -eq 0 ] ; then
+  /bin/echo "Entering environment at '/home/viki/RosWorkspace/Project/hovercraft/build/devel', type 'exit' to leave"
+  . "/home/viki/RosWorkspace/Project/hovercraft/build/devel/setup.sh"
+  "$SHELL" -i
+  /bin/echo "Exiting environment at '/home/viki/RosWorkspace/Project/hovercraft/build/devel'"
+else
+  . "/home/viki/RosWorkspace/Project/hovercraft/build/devel/setup.sh"
+  exec "$@"
+fi
---
+++ lab1/hovercraft/msg_gen/lisp/_package.lisp
@@ -0,0 +1,15 @@
+(cl:defpackage hovercraft-msg
+  (:use )
+  (:export
+   "<BATTERYVOLTAGE>"
+   "BATTERYVOLTAGE"
+   "<THRUSTER>"
+   "THRUSTER"
+   "<CURRENT>"
+   "CURRENT"
+   "<GYRO>"
+   "GYRO"
+   "<LED>"
+   "LED"
+  ))
+
