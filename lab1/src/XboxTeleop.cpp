204c7ab83721eddf39736f305ab66cae2b752ee4
Tony Schneider <aschneid@cse.unl.edu>
2013-09-17 14:36:35 -0500

    Fixed spacing and indentation (aligned stuff, made other stuff consistent)

--- lab1/src/XboxTeleop.cpp
+++ lab1/src/XboxTeleop.cpp
@@ -1,5 +1,5 @@
 /* Created by Tony Schneider on 9/12/2013
- * 
+ *
  * XboxTeleop.cpp
  *
  * This node is used to receive messages from the
@@ -15,94 +15,94 @@
 class XboxTeleop
 {
 public:
-  XboxTeleop();
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+    XboxTeleop();
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
 private:
-  ros::NodeHandle nh_;
-  ros::Publisher thruster_pub;
-  ros::Subscriber joy_sub;
-  bool liftOn;
-  bool startButtonDepressed;
-  const double liftPower;
-  const double thrustModifier;
+    ros::NodeHandle nh_;
+    ros::Publisher thruster_pub;
+    ros::Subscriber joy_sub;
+    bool liftOn;
+    bool startButtonDepressed;
+    const double liftPower;
+    const double thrustModifier;
 };
 
 XboxTeleop::XboxTeleop() : liftPower (.4), thrustModifier(.5)
 {
-  thruster_pub = nh_.advertise<hovercraft::Thruster>("hovercraft/Thruster", 1);
-  joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &XboxTeleop::joyCallback, this);
-  liftOn = false;
-  startButtonDepressed = false;
-  liftPower = .4;
-  thrustModifier = .5;
+    thruster_pub = nh_.advertise<hovercraft::Thruster>("hovercraft/Thruster", 1);
+    joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &XboxTeleop::joyCallback, this);
+    liftOn = false;
+    startButtonDepressed = false;
+    liftPower = .4;
+    thrustModifier = .5;
 }
 
 void XboxTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  hovercraft::Thruster thrust;
-  ROS_DEBUG("joyCallback executed");
-  printf("joyCallback executed\n");
-  
-  if (joy->buttons[7] == 1 and not startButtonDepressed)
+    hovercraft::Thruster thrust;
+    ROS_DEBUG("joyCallback executed");
+    printf("joyCallback executed\n");
+
+    if (joy->buttons[7] == 1 and not startButtonDepressed)
     {
-	startButtonDepressed = true;
-	ROS_DEBUG("Start Button Pressed\n");
-	printf("Start Button Pressed\n");
-	thrust.thruster1 = 0;
-	thrust.thruster2 = 0;
-	thrust.thruster3 = 0;
-	thrust.thruster4 = 0;
-	thrust.thruster5 = 0;
-	if (liftOn)
-	{
-	  printf("Turning Thruster Off\n");
-	  ROS_DEBUG("Turning Thruster Off\n");
-	  liftOn = false;
-	  thrust.lift = 0;
-	}
-      else
-	{
-          printf("Turning Thruster On\n");
-	  ROS_DEBUG("Turning Thruster On\n");
-	  thrust.lift = liftPower;
-	  liftOn = true;
-	}
-  	thruster_pub.publish(thrust);  
-    }
-  else if (joy->buttons[7] == 0 and startButtonDepressed)
-    { 
-	startButtonDepressed = false;
-    }
-  
-  if (liftOn)
-  {	
-	thrust.lift = liftPower;
-	if (joy->axes[0] < 0)
+        startButtonDepressed = true;
+        ROS_DEBUG("Start Button Pressed\n");
+        printf("Start Button Pressed\n");
+        thrust.thruster1 = 0;
+        thrust.thruster2 = 0;
+        thrust.thruster3 = 0;
+        thrust.thruster4 = 0;
+        thrust.thruster5 = 0;
+        if (liftOn)
         {
-		thrust.thruster4 = 0;
-		thrust.thruster5 = fabs(joy->axes[0]) * thrustModifier;
-        } 
-	else if(joy->axes[0] > 0)
-	{
-		thrust.thruster5 = 0;
-		thrust.thruster4 = joy->axes[0] * thrustModifier;
-	}
+            printf("Turning Thruster Off\n");
+            ROS_DEBUG("Turning Thruster Off\n");
+            liftOn = false;
+            thrust.lift = 0;
+        }
         else
-	{
-		thrust.thruster5 = 0;
-		thrust.thruster4 = 0;
-	}
-  	thruster_pub.publish(thrust);  
-  }
- 
+        {
+            printf("Turning Thruster On\n");
+            ROS_DEBUG("Turning Thruster On\n");
+            thrust.lift = liftPower;
+            liftOn = true;
+        }
+        thruster_pub.publish(thrust);
+    }
+    else if (joy->buttons[7] == 0 and startButtonDepressed)
+    {
+        startButtonDepressed = false;
+    }
+
+    if (liftOn)
+    {
+        thrust.lift = liftPower;
+        if (joy->axes[0] < 0)
+        {
+            thrust.thruster4 = 0;
+            thrust.thruster5 = fabs(joy->axes[0]) * thrustModifier;
+        }
+        else if(joy->axes[0] > 0)
+        {
+            thrust.thruster5 = 0;
+            thrust.thruster4 = joy->axes[0] * thrustModifier;
+        }
+        else
+        {
+            thrust.thruster5 = 0;
+            thrust.thruster4 = 0;
+        }
+        thruster_pub.publish(thrust);
+    }
+
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "XboxTeleop");
-  XboxTeleop xbox_teleop;
-  ROS_DEBUG("Xbox Teleop spinning");
-  printf("Xbox Teleop spinning\n");
-  ros::spin();
+    ros::init(argc, argv, "XboxTeleop");
+    XboxTeleop xbox_teleop;
+    ROS_DEBUG("Xbox Teleop spinning");
+    printf("Xbox Teleop spinning\n");
+    ros::spin();
 }
