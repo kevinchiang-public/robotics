adff6e32a253531fa23880e6072b9f1742b96dd2
Tony Schneider <aschneid@cse.unl.edu>
2013-10-12 16:22:57 -0500

    Changed rotational angle to map 0 to +Y-axis instead of +X-axis (makes more sense in relation to the hovercraft).  We'll need to add in some logic to the angle integrator PID to choose the shortest path to the correct angle, and to take into account the difference between the input from the gyro (which might start at any arbitrary number, not just one capped from [0,360]

--- lab2/src/angleIntegrator.py
+++ lab2/src/angleIntegrator.py
@@ -7,23 +7,26 @@ from lab2.msg import MovementRaw, Movement
 import math
 
 def listener():
-	rospy.init_node('JoystickAngleIntegrator')
-	rospy.Subscriber('/joyOut', MovementRaw, callback)
-	rospy.spin()
+        rospy.init_node('JoystickAngleIntegrator')
+        rospy.Subscriber('/joyOut', MovementRaw, callback)
+        rospy.spin()
 
 def callback(move):
-	publisher = rospy.Publisher('/angleIntegratorOut',Movement)
-	xAxisL = move.xL
-	yAxisL = move.yL
-	xAxisR = move.xR
-	xAxisR = move.yR
+        publisher = rospy.Publisher('/angleIntegratorOut',Movement)
+        xAxisL = move.xL
+        yAxisL = move.yL
+        xAxisR = move.xR
+        xAxisR = move.yR
 
-	
-	#Get the arctangent of yAxis/xAxis to get the angle in radians.
+
+        #Get the arctangent of xAxis/yAxis to get the angle in radians.
         #Convert it to degrees and make it so that it goes from 0-360 starting
-        #at the positive x-axis
-	rotationalAngle = round(math.atan2(yAxisL,xAxisL)*(180.0/3.141593)+180,4)
-	
-	
+        #at the positive y axis (to match with the front of the hovercraft)
+        rotationalAngle = round(math.atan2(xAxisL,yAxisL)*(180.0/3.141593),4)
+        if (rotationalAngle > 0):
+                rotationalAngle = rotationalAngle - 360
+        rotationalAngle = math.fabs(rotationalAngle)
+        print "xAxisL: " + str(xAxisL) + "\tyAxisL: " + str(yAxisL) + "\tRotational Angle: " + str(rotationalAngle)
+
 if __name__ == '__main__':
-        listener() 
+        listener()
