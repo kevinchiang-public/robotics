3deb711405c9be24390bd54847b37d894f8d123d
Tony Schneider <aschneid@cse.unl.edu>
2013-10-12 16:42:27 -0500

    Added magnitude to implement a heft deadzone for the angle.  Will probably want to incorporate the magnitude into the target rotational rate at some point.  Also fixed a couple of minor errors

--- lab2/src/angleIntegrator.py
+++ lab2/src/angleIntegrator.py
@@ -16,17 +16,24 @@ def callback(move):
         xAxisL = move.xL
         yAxisL = move.yL
         xAxisR = move.xR
-        xAxisR = move.yR
-
+        yAxisR = move.yR
 
         #Get the arctangent of xAxis/yAxis to get the angle in radians.
         #Convert it to degrees and make it so that it goes from 0-360 starting
-        #at the positive y axis (to match with the front of the hovercraft)
-        rotationalAngle = round(math.atan2(xAxisL,yAxisL)*(180.0/3.141593),4)
-        if (rotationalAngle > 0):
+        #at the positive y axis (to match with the front of the hovercraft).
+        #Uses extreme deadzone to makesure accidental rotations don't happen.
+        #We'll want to incorporate the magnitude later
+        #to control target velocity maybe (as a multiplier, perhaps?)
+        magnitude = math.sqrt(xAxisL**2 + yAxisL**2)
+        rotationalAngle = 0
+        if magnitude >= 1:
+            rotationalAngle = round(math.atan2(xAxisL,yAxisL)*(180.0/3.141593),4)
+            if (rotationalAngle > 0):
                 rotationalAngle = rotationalAngle - 360
-        rotationalAngle = math.fabs(rotationalAngle)
-        print "xAxisL: " + str(xAxisL) + "\tyAxisL: " + str(yAxisL) + "\tRotational Angle: " + str(rotationalAngle)
+            rotationalAngle = math.fabs(rotationalAngle)
+        print("xL: %6.2f  yL: %6.2f  Rotational angle: %6.2f  Magnitude:%6.2f  "
+              "xR: %6.2f  yR: %6.2f" % (xAxisL,yAxisL,rotationalAngle,
+                                        magnitude,xAxisR,yAxisR))
 
 if __name__ == '__main__':
         listener()
