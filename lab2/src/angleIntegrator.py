14fbab832f0239f942922b4c37e60a35aed03ffd
Tony Schneider <aschneid@cse.unl.edu>
2013-10-12 17:15:40 -0500

    Added debug parameter for easy debug on/off switch.  Probably changed some other crap that I can't remember now, too.  Basic functionality is still the same.  Parameters can be rosrun'd by adding _paramname:=paramvalue to the command

--- lab2/src/angleIntegrator.py
+++ lab2/src/angleIntegrator.py
@@ -22,7 +22,7 @@ def callback(move):
         #Convert it to degrees and make it so that it goes from 0-360 starting
         #at the positive y axis (to match with the front of the hovercraft).
         #Uses extreme deadzone to makesure accidental rotations don't happen.
-        #We'll want to incorporate the magnitude later
+        #We may want to incorporate the magnitude later
         #to control target velocity maybe (as a multiplier, perhaps?)
         magnitude = math.sqrt(xAxisL**2 + yAxisL**2)
         rotationalAngle = 0
@@ -31,9 +31,20 @@ def callback(move):
             if (rotationalAngle > 0):
                 rotationalAngle = rotationalAngle - 360
             rotationalAngle = math.fabs(rotationalAngle)
-        print("xL: %6.2f  yL: %6.2f  Rotational angle: %6.2f  Magnitude:%6.2f  "
-              "xR: %6.2f  yR: %6.2f" % (xAxisL,yAxisL,rotationalAngle,
-                                        magnitude,xAxisR,yAxisR))
+
+        #Prints all information related to the integrator if need be
+        debug = rospy.get_param('~debug', '0')
+        if (debug == 1):
+            print("xL: %6.2f  yL: %6.2f  Angle: %6.2f  Magnitude:%6.2f  "
+                  "xR: %6.2f  yR: %6.2f" % (xAxisL,yAxisL,rotationalAngle,
+                                            magnitude,xAxisR,yAxisR))
+
+        #Ships off the message to the arbitrator
+        moveOut = Movement()
+        moveOut.theta = rotationalAngle
+        moveOut.x = xAxisR
+        moveOut.y = yAxisR
+        publisher.publish(moveOut)
 
 if __name__ == '__main__':
         listener()
