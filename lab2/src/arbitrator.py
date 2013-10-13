d89626542b63d1a5c6609126591135dd1bd13ffe
Tony Schneider <aschneid@cse.unl.edu>
2013-10-13 16:11:24 -0500

    Converted to class format, restructured some logic

--- lab2/src/arbitrator.py
+++ lab2/src/arbitrator.py
@@ -2,95 +2,53 @@
 import rospy
 import roslib
 roslib.load_manifest('lab2')
-roslib.load_manifest('hovercraft')
+
 from sensor_msgs.msg import Joy
 from hovercraft.msg import Thruster
 from lab2.msg import Switcher, MovementRaw, Movement
 
-state = 0
-liftOn  = False
-currentThrust = Thruster()
-def thrustersOn(thrust):
-	if thrust.lift > 0:
-		return True
-	elif thrust.thruster1 > 0:
-		return True
-	elif thrust.thruster2 > 0:
-		return True
-	elif thrust.thruster3 > 0:
-		return True
-	elif thrust.thruster4 > 0:
-		return True
-	elif thrust.thruster5 > 0:
-		return True
-	else:
-		return False
 
-def callback(switcher):
-	global state
-	global liftOn
-	global currentThrust
-	'''
-	JOYSTICK = 0
-	TRIANGLE = 1
-	REACTIVE = 2
-	'''
+class Arbitrator():
+    def __init__(self):
+        self.debug = rospy.get_param('~debug', '0')
+        self.movement = Movement()
+        self.liftOn = False
+        self.state = 'Manual'
+        rospy.init_node('Arbitrator')
+        rospy.Subscriber('/joyArbitrator', Switcher, self.joyCallback)
+        rospy.Subscriber('/angleIntegratorOut', Movement, self.manualJoyControl)
+        #rospy.Subscriber('/triangleOut', Movement?, triangleCallback)
+        #rospy.Subscriber('/reactiveOut', Movement?, reactiveCallback)
 
-	state = switcher.state
-	liftOn  = switcher.lift
-	publisher = rospy.Publisher('/arbitratorThruster',Thruster)
-	thrust = Thruster()
-	if not liftOn and thrustersOn(currentThrust):
-		offPub = rospy.Publisher('/hovercraft/Thruster',Thruster)
-		thrust.lift = 0 
-		thrust.thruster1=0
-		thrust.thruster2=0
-		thrust.thruster3=0
-		thrust.thruster4=0
-		thrust.thruster5=0
-		offPub.publish(thrust)
-	elif liftOn:
-		thrust.lift = .3	
-		thrust.thruster1=0
-		thrust.thruster2=0
-		thrust.thruster3=0
-		thrust.thruster4=0
-		thrust.thruster5=0
-	publisher.publish(thrust)
-	'''
-	if liftOn:
-		if state == JOYSTICK:
-			pass #TODO implement what happens during joystick control
-		elif state == TRIANGLE:
-			pass #TODO see above
-		elif state == REACTIVE:
-			pass #TODO see above
-	'''
-	
-def manualCallback(move):
-	global state
-	publisher = rospy.Publisher('/arbitratorOut',Movement)
-	if liftOn and state==JOYSTICK:
-		publisher.publish(move)
-		
-def triangleCallback():
-	pass #TODO implement this.  The logic should be similar to manualCallback
+    def joyCallback(self, switch):
+        if (switch.state == 0):
+            self.state = 'Manual'
+        elif (switch.state == 1):
+            self.state = 'Triangle'
+        elif (switch.state == 2):
+            self.state = 'Reactive'
 
-def reactiveCallback():
-	pass #TODO see above
+        publisher = rospy.Publisher('/arbitratorOut', Movement)
+        if not liftOn:
+            self.liftOn = true
+            publisher.publish(Movement(0,0,0))
+        elif liftOn:
+            publisher.publish(self.movement)
 
-def thrusterCallback(thruster):
-	global currentThrust
-	currentThrust = thruster
+    def manualJoyControl(self, move):
+        if self.state is 'Manual':
+            self.movement = move
 
-def listener():
-	rospy.init_node('arbitrator')
-	rospy.Subscriber('/hovercraft/Thruster',Thruster, thrusterCallback)
-	rospy.Subscriber('/joyArbitrator', Switcher, callback)
-	rospy.Subscriber('/angleIntegratorOut', Movement, manualCallback)
-	#rospy.Subscriber('/triangleOut', Type, triangleCallback)
-	#rospy.Subscriber('/reactiveOut', Type, reactiveCallback)
-	rospy.spin()
+    def triangleCallback(self, move):
+        if self.state is 'Triangle':
+            self.movement = move
+
+    def reactiveCallback(self, move):
+        if self.state is 'Reactive':
+            self.movement = move
 
 if __name__ == '__main__':
-	listener()
+    try:
+        ne = Arbitrator()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
