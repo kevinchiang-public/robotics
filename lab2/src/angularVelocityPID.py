639ae875d78a7eb4ddb96e5daa3cec5de0bc63f2
Tony Schneider <aschneid@cse.unl.edu>
2013-10-14 17:07:41 -0500

    Converted to class form

--- lab2/src/angularVelocityPID.py
+++ lab2/src/angularVelocityPID.py
@@ -1,58 +1,52 @@
 #!/usr/bin/env python
 import rospy
 import roslib
+import math
 roslib.load_manifest('lab2')
 roslib.load_manifest('hovercraft')
 from hovercraft.msg import Gyro
-from sensor_msgs.msg import Joy
 from lab2.msg import Movement
-import math
 
-DEBUG = rospy.get_param('~debug',False)
-xDepressed = False
-bDepressed = False
-#targetRate= float(rospy.get_param('~targetRate','10'))
-targetRate=0
-previousError=0
-movePass=Movement()
-def printD(string):
-	global DEBUG
-	if DEBUG:
-		print string
+class AngularVelocityPID():
+    def __init__(self):
+        self.debug = float(rospy.get_param('~debug', '0'))
+        self.targetRate = 0
+        self.previousError = 0
+        self.movePass = Movement()
+        rospy.init_node('AngularVelocityPID')
+        rospy.Subscriber('/hovercraft/Gyro', Gyro, self.gyroCallback)
+        rospy.Subscriber('/angularPositionOut', Movement, self.positionPIDCallback)
 
-def listener():
-	rospy.init_node('AngularVelocityPID')
-	rospy.Subscriber('/hovercraft/Gyro',Gyro,gyroCallback)
-	rospy.Subscriber('/angularPositionOut',Movement,positionCallback)
-	rospy.spin()
+    def printDebug(self, stringToPrint):
+        if self.debug == 1:
+            print(stringToPrint)
 
-def positionCallback(move):
-	global targetRate
-	global movePass
-	movePass = move
-	targetRate = move.theta
+    def positionPIDCallback(self, move):
+        self.movePass = move
+        self.targetRate = move.theta
 
-def gyroCallback(gyro):
-	global targetRate
-	global previousError
-	global DEBUG
-	global movePass
-	pub = rospy.Publisher('/thrusterMapping',Movement)
-	move = Movement()
-	P = float(rospy.get_param('~P', '.01'))
-	D = float(rospy.get_param('~D', '.01'))
-	printD(P)
-	r = P*(targetRate - gyro.rate)+D*((targetRate - gyro.rate)-previousError)
-	#TODO Derivative part
-	previousError = targetRate - gyro.rate
-	if DEBUG:
-		print "TargetRate:",targetRate,"\tGyro Rate:",gyro.rate,"\tr:",r,"\tRate Difference:",previousError
-	move.theta = r #if math.fabs(targetAngle - gyro.angle) > 2 else 0
-	if math.fabs(targetRate - gyro.rate) < 3:
-		move.theta = 0
-	move.x=movePass.x
-	move.y=movePass.y
-	pub.publish(move)
+    def gyroCallback(self, gyro):
+        pub = rospy.Publisher('/thrusterMapping',Movement)
+        move = Movement()
+        P = float(rospy.get_param('~P', '.01'))
+        D = float(rospy.get_param('~D', '.01'))
+
+        r = P*(self.targetRate - gyro.rate)+D*((self.targetRate - gyro.rate)-self.previousError)
+        self.previousError = self.targetRate - gyro.rate
+
+        self.printDebug('TargetRate:{:5.3f}  Gyro Rate:{:5.3f}  RateDiff:{:5.3f}'.format(
+            self.targetRate, gyro.rate, self.previousError))
+
+        move.theta = r
+        if math.fabs(targetRate - gyro.rate) < 3:
+                move.theta = 0
+        move.x=movePass.x
+        move.y=movePass.y
+
+        pub.publish(move)
 
 if __name__ == '__main__':
-	listener() 
+    try:
+        ne = Arbitrator()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
