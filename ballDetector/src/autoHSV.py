0580ee001425c4869ca255f208a76a26f542055f
Dopey <dopey@dopey-ROS.(none)>
2013-10-30 22:29:39 -0500

    Kind of crappy auto calibration

--- ballDetector/src/autoHSV.py
+++ ballDetector/src/autoHSV.py
@@ -3,6 +3,7 @@ import rospy
 import roslib
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Vector3
+import numpy
 class AutoHSV():
 	def __init__(self):
 		self.threshHighPub = rospy.Publisher('thresh/high',Vector3)
@@ -16,23 +17,37 @@ class AutoHSV():
 		V = []
 		#Seperate the image into HSV values
 		for x in xrange(0, len(image.data), 3):
-			if image.data[x] < 64 or image.data[x] > 108:
-				H.append(ord(image.data[x]))
-		for x in xrange(1, len(image.data), 3):
-			if image.data[x] < 10 or image.data[x] > 70:
-				S.append(ord(image.data[x]))
-		for x in xrange(2, len(image.data), 3):
-			if image.data[x] < 105 or image.data[x] > 154:
-				V.append(ord(image.data[x]))
-		#print H
-		
+			if ord(image.data[x+1]) < 10 or ord(image.data[x+1]) > 70:
+				if ord(image.data[x+2]) < 105 or ord(image.data[x+2]) > 154:
+					H.append(ord(image.data[x]))
+					S.append(ord(image.data[x+1]))
+					V.append(ord(image.data[x+2]))		
+		'''	
 		lh = min(H)
 		ls = min(S)
 		lv = min(V)
 		hh = max(H)
 		hs = max(S)
 		hv = max(V)
-		#print ("LH:%d\tLS:%d\tLV:%d\tHH:%d\tHS:%d\tHV:%d"%(lh, ls, lv, hh, hs, hv))
+		print ("LH:%d\tLS:%d\tLV:%d\tHH:%d\tHS:%d\tHV:%d"%(lh, ls, lv, hh, hs, hv))
+		'''
+		hAvg = numpy.mean(H)
+		sAvg = numpy.mean(S)
+		vAvg = numpy.mean(V)
+
+		hStd = numpy.std(H)
+		sStd = numpy.std(S)
+		vStd = numpy.std(V)
+
+		
+		lh = hAvg - hStd
+		ls = sAvg - sStd
+		lv = vAvg - vStd
+		hh = hAvg + hStd
+		hs = sAvg + sStd
+		hv = vAvg + vStd
+		print ("LH:%f\tLS:%f\tLV:%f\tHH:%f\tHS:%f\tHV:%f"%(lh, ls, lv, hh, hs, hv))
+
 		self.threshHighPub.publish(x=hh,y=hs,z=hv)
 		self.threshLowPub.publish(x=lh,y=ls,z=lv)
 
