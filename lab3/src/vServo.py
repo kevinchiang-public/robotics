bc165d2f35f503e5759e2c8d612c5228734f3ba2
Dopey <kevinchiang@outlook.com>
2013-11-09 15:12:18 -0600

    Visual servo Y axis working, but needs to be tuned

--- lab3/src/vServo.py
+++ lab3/src/vServo.py
@@ -16,44 +16,52 @@ class CameraIntegrator():
         #distance from camera to the front most point of hovercraft is 15 cm, the mimal camera   distance is 36 cm
         self.targetDistance = float(rospy.get_param('~Distance', '40'))
         self.move = Movement()
-        rospy.Subscriber('/landmarkLocation', landmarkLocation, self.integrateRawValues) #Probably wrong
+        rospy.Subscriber('/landmarkLocation', landmarkLocation, self.integrateRawValues)
 
     def integrateRawValues(self, landmarkLoc):
         #Grab data from camera
-        centerX=(landmarkLoc.xtop+landmarkLoc.xbottom)/2
-        centerY=(landmarkLoc.ytop+landmarkLoc.ybottom)/2
+        centerX=(landmarkLoc.xtop+landmarkLoc.xbottom)/2.0
+        centerY=(landmarkLoc.ytop+landmarkLoc.ybottom)/2.0
         height=landmarkLoc.height
         code=landmarkLoc.code
 
         #Calculate actual distance using interpolated polynomial
-        distance=self.calcDistance(height)
+        distance = 0
+        if (height != 0):
+            distance=self.calcDistance(height)
 
-        #PI control for y axis only
+        #PD control for y axis only
         currentDistance=distance
         targetDistance=self.targetDistance
         r = self.P*(targetDistance-currentDistance)
         r = r + self.D*((targetDistance - currentDistance) - self.previousError)
 
         self.previousError = targetDistance - currentDistance
-
-        #deadband
+        self.debugPrint("R before: %6.2f" % r)
+        #Deadband
         if math.fabs(targetDistance - currentDistance) < 5:
             r = 0
 
 
-        self.debugPrint("R: %6.2f Target Distance: %6.2f    Current Distance: %6.2f   Previous Error: %6.2f"%(-r, targetDistance,currentDistance,self.previousError))
-        r = r/10.0
+        self.debugPrint("R: %6.2f Target Distance: %6.2f    Current Distance: %6.2f "
+                        "Previous Error: %6.2f Height: %6.2f"%
+                        (-r, targetDistance,currentDistance,self.previousError, height))
 
+        #Arbitrary division to get r into appropriate thruster range
+        #(determined via some trial-and-error experimentation)
+        r = r/50.0
         if r > 1:
             r = 1
         elif r < -1:
             r = -1
 
+        #If we get a bad height reading, just publish the previous move message
         publisher = rospy.Publisher('/visualServoOut', Movement)
-        self.move.theta = 0
-        self.move.y = -r
-        self.move.x = 0
-        self.move.modType = 'Add'
+        if (height != 0):
+            self.move.theta = 0
+            self.move.y = -r
+            self.move.x = 0
+            self.move.modType = 'Add'
         publisher.publish(self.move)
 
     def calcDistance(self, rawValue):
