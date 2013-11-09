0362f90a888193eb2050845cfa8dd9e755db9a6f
Dopey <kevinchiang@outlook.com>
2013-11-09 15:48:56 -0600

    Added a timer to call the move function so the hovercraft comintues to move in the same direction if the target landmark is lost.  Also added a paramter to choose a target landmark (set to -1 to follow any landmark).  Note: Still need to calculate a target angle to center the hovercraft on the landmark.  Not sure how to do this with the information given, if it's even possible.  May want to ask Carrick if it's necessary

--- lab3/src/vServo.py
+++ lab3/src/vServo.py
@@ -10,13 +10,28 @@ import math
 class CameraIntegrator():
     def __init__(self):
         self.debug = float(rospy.get_param('~debug', '1'))
-        self.previousError=0
+        self.previousYError=0
         self.P = float(rospy.get_param('~P','1'))
         self.D = float(rospy.get_param('~D','0'))
+        self.centerX = 0
+        self.centerY = 0
+        self.height = 0
+        self.code = -2
+
         #distance from camera to the front most point of hovercraft is 15 cm, the mimal camera   distance is 36 cm
         self.targetDistance = float(rospy.get_param('~Distance', '40'))
+
+        #The landmark to find.  If set to -1, it will follow any landmark visible
+        #Two landmarks visible at once with -1 will result in undefined behavior
+        self.targetLandmark = float(rospy.get_param('~LandmarkNumber', '-1'))
         self.move = Movement()
+        rospy.Timer(rospy.Duration(.1), self.moveHovercraft)
         rospy.Subscriber('/landmarkLocation', landmarkLocation, self.integrateRawValues)
+
+    #Set on a timer to continue moving the hovercraft
+    #in the last set direction in case the landmark is lost
+    def moveHovercraft(self):
+        publisher.publish(self.move)
 
     def integrateRawValues(self, landmarkLoc):
         #Grab data from camera
@@ -27,23 +42,32 @@ class CameraIntegrator():
 
         #Calculate actual distance using interpolated polynomial
         distance = 0
-        if (height != 0):
+        if height != 0:
             distance=self.calcDistance(height)
 
+        #Get the target translational thrust in Y direction
+        yR = yPID(distance)
+
+        #If we get a bad height reading, just publish the previous move message
+        publisher = rospy.Publisher('/visualServoOut', Movement)
+        if (height != 0 and (code == targetLandmark or targetLandmark == -1):
+            self.move.theta = self.getTargetAngle(0) #Change this later (when we figure out how to do it)
+            self.move.y = -r
+            self.move.x = 0
+            self.move.modType = 'Add'
+
+    def yPID(self, currentDistance):
         #PD control for y axis only
-        currentDistance=distance
         targetDistance=self.targetDistance
         r = self.P*(targetDistance-currentDistance)
-        r = r + self.D*((targetDistance - currentDistance) - self.previousError)
+        r = r + self.D*((targetDistance - currentDistance) - self.previousYError)
+        self.previousYError = targetDistance - currentDistance
 
-        self.previousError = targetDistance - currentDistance
-        self.debugPrint("R before: %6.2f" % r)
         #Deadband
         if math.fabs(targetDistance - currentDistance) < 5:
             r = 0
 
-
-        self.debugPrint("R: %6.2f Target Distance: %6.2f    Current Distance: %6.2f "
+        self.debugPrint("yR: %6.2f Target Distance: %6.2f    Current Distance: %6.2f "
                         "Previous Error: %6.2f Height: %6.2f"%
                         (-r, targetDistance,currentDistance,self.previousError, height))
 
@@ -54,15 +78,12 @@ class CameraIntegrator():
             r = 1
         elif r < -1:
             r = -1
+        return r
 
-        #If we get a bad height reading, just publish the previous move message
-        publisher = rospy.Publisher('/visualServoOut', Movement)
-        if (height != 0):
-            self.move.theta = 0
-            self.move.y = -r
-            self.move.x = 0
-            self.move.modType = 'Add'
-        publisher.publish(self.move)
+    #Determines the target angle of the hovercraft based on
+    #the center of the landmark w.r.t. the entire camera image
+    def getTargetAngle(self, center): #TODO: IMPLEMENT
+            return 0.0
 
     def calcDistance(self, rawValue):
         returnVal = 7334.9 * math.pow(rawValue, -0.996)
