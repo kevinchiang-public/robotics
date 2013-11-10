1af626116724d461727dec2e2b0785af8712d803
Kevin Chiang <kevinchiang@outlook.com>
2013-11-10 14:59:20 -0600

    Implemented visionArbitrator, edited CMakeList for message generation, added detectionSwticher

--- lab3/src/visionArbitrator.py
+++ lab3/src/visionArbitrator.py
@@ -2,17 +2,35 @@
 import rospy
 import roslib
 roslib.load_manifest('lab3')
+roslib.load_manifest('ballDetector')
 
-from sensor_msgs.msg import Joy
+from ballDetector.msg import ballLocation
+from sensor_msgs.msg import Joy, Image
 from hovercraft.msg import Thruster
-from lab3.msg import Switcher, MovementRaw, Movement
+from lab3.msg import Switcher, MovementRaw, Movement, DetectionSwitcher
 from copy import deepcopy as deep
 class VisionArbitrator():
     def __init__(self):
         #Idea: intercept topic that typically publishes to ball detector/landmark using remapping in the
         #launch file.  Then publish out to one or the other pending on the switch state from the
         #switch node.
+        self.publishTarget = None
+        self.ballPublisher = rospy.Publisher('/vis/Arbitrator/toBall',Image)
+        self.landPublisher = rospy.Publisher('/vis/Arbitrator/toLand',Image)
+        rospy.Subscriber('/vis/Arbitrator/cameraIn', Image, self.cameraCallback)
+        rospy.Subscriber('/vis/Arbitrator/switcher', DetectionSwitcher, self.switcherCallback)
 
+    def cameraCallback(self, image):
+        if publishTarget is 'ballDetector':
+            self.ballPublisher.publish(image)
+        elif publishTarget is 'landDetector':
+            self.landPublisher.publish(image)
+    def switcherCallback(self, switcher):
+        if switcher.state == 0:
+            self.publishTarget = 'ballDetector'
+        elif switcher.state == 1:
+            self.publishTarget = 'landDetector'
+        
 if __name__ == '__main__':
     rospy.init_node('VisionArbitrator')
     try:
