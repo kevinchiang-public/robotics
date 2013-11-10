bceba277c2a57f9a5d5e641fbdfcfceee780c91a
Dopey <kevinchiang@outlook.com>
2013-11-10 16:07:05 -0600

    reverse landmarks = Yay

--- landmarkSelfSim/src/landmarkDetector.cpp
+++ landmarkSelfSim/src/landmarkDetector.cpp
@@ -59,12 +59,12 @@ private:
 LandmarkDetector::LandmarkDetector()
   :it(nh)
   {
-    image_sub_ = it.subscribe("image", 1, &LandmarkDetector::imageCb, this);
+	image_sub_ = it.subscribe("image", 1, &LandmarkDetector::imageCb, this);
 
-    //Debug image publisher
-    outImg_pub_ = it.advertise("landmark/outImg",1);
-    //Landmark position publisher
-    landmark_pub_ = nh.advertise<landmarkSelfSim::landmarkLocation>("landmarkLocation",1);
+	//Debug image publisher
+	outImg_pub_ = it.advertise("landmark/outImg",1);
+	//Landmark position publisher
+	landmark_pub_ = nh.advertise<landmarkSelfSim::landmarkLocation>("landmarkLocation",1);
   }
 
 /**
@@ -80,9 +80,9 @@ GrayImage cvToGrayImage(cv::Mat img){
   GrayImage retImg = newGrayImage(grayImg.cols,grayImg.rows);
 
   for(int i=0; i<grayImg.rows; i++){
-    for(int j=0; j<grayImg.cols; j++){
-      imRef(retImg,j,i) = grayImg.at<uchar>(i,j);
-    }
+	for(int j=0; j<grayImg.cols; j++){
+	  imRef(retImg,j,i) = grayImg.at<uchar>(i,j);
+	}
   }
 
   return retImg;
@@ -90,127 +90,139 @@ GrayImage cvToGrayImage(cv::Mat img){
 
 
 void LandmarkDetector::imageCb(const sensor_msgs::ImageConstPtr& msg){
-    cv_bridge::CvImagePtr cv_ptr;
-    try{
-        cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-    }catch (cv_bridge::Exception& e){
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
+	cv_bridge::CvImagePtr cv_ptr;
+	try{
+		cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+	}catch (cv_bridge::Exception& e){
+		ROS_ERROR("cv_bridge exception: %s", e.what());
+		return;
+	}
 
-    //Get a the image just as a Mat
-    cv::Mat colorImg = cv_ptr->image;//.clone();
-    GrayImage im1, rim1;
-    landmarks lamarr, lamarrRev;
-    double factor = FACTOR_DEFAULT;
-    int spacing = SPACING_DEFAULT;
-    int window = WINDOW_DEFAULT;
-    int threshold = THRESHOLD_DEFAULT;
-    int peakperc = PEAKW_PERCENT;
-    int skip = SKIP_DEFAULT;
-    int ycoord = -1;
-    int drawX = 0;
-    int verbose = 0;
-    int peakw;
-    peakw = (peakperc * window) / 100 + PEAKW_ADD;
+	//Get a the image just as a Mat
+	cv::Mat colorImg = cv_ptr->image;//.clone();
+	GrayImage im1, rim1;
+	landmarks lamarr, lamarrRev;
+	double factor = FACTOR_DEFAULT;
+	int spacing = SPACING_DEFAULT;
+	int window = WINDOW_DEFAULT;
+	int threshold = THRESHOLD_DEFAULT;
+	int peakperc = PEAKW_PERCENT;
+	int skip = SKIP_DEFAULT;
+	int ycoord = -1;
+	int drawX = 0;
+	int verbose = 0;
+	int peakw;
+	peakw = (peakperc * window) / 100 + PEAKW_ADD;
 
-    //Get the mcimg formated image
-    im1 = cvToGrayImage(colorImg);
-    rim1 = cvToGrayImage(colorImg);
-    for (int i = 0; i < im1->height; i++)
-      for (int j = 0; j < im1->width; j++)
-           rim1->data[im1->height - i - 1][im1->width - j - 1] = im1->data[i][j];
+	//Get the mcimg formated image
+	im1 = cvToGrayImage(colorImg);
+	rim1 = cvToGrayImage(colorImg);
 
-    lamarr = landmarkParams(im1, NULL, //"out.match.ppm",
-                            verbose, drawX,
-                            window, peakw, threshold,
-                            factor, spacing, skip, ycoord);
-    lamarrRev = landmarkParams(rim1, NULL, //"out.match.ppm",
-                               verbose, drawX,
-                               window, peakw, threshold,
-                               factor, spacing, skip, ycoord);
+	//Flips the upside down image
+	for (int i = 0; i < im1->height; i++)
+	  for (int j = 0; j < im1->width; j++)
+		   rim1->data[im1->height - i - 1][im1->width - j - 1] = im1->data[i][j];
+
+	lamarr = landmarkParams(im1, NULL, //"out.match.ppm",
+							verbose, drawX,
+							window, peakw, threshold,
+							factor, spacing, skip, ycoord);
+	lamarrRev = landmarkParams(rim1, NULL, //"out.match.ppm",
+							   verbose, drawX,
+							   window, peakw, threshold,
+							   factor, spacing, skip, ycoord);
 //Free the image
-    imFree(im1);
-    imFree(rim1);
+	imFree(im1);
+	imFree(rim1);
 
-    //Create a smaller image that we will mark up
+	//Create a smaller image that we will mark up
 #define SCALE_FACTOR 1.0
-    cv::Mat smlImg = cv::Mat();
-    cv::resize(colorImg,smlImg,cv::Size(),SCALE_FACTOR,SCALE_FACTOR,
-               cv::INTER_NEAREST);
+	cv::Mat smlImg = cv::Mat();
+	cv::resize(colorImg,smlImg,cv::Size(),SCALE_FACTOR,SCALE_FACTOR,
+			   cv::INTER_NEAREST);
 
-    //Copy the reverse landmarks found into lamarr
-    for (int i = 0; i < lamarrRev.count; i++)
-    {
-        if (lamarr.count < 99)
-        {
-            lamarr.lm[lamarr.count] = lamarrRev.lm[i];
-            lamarr.count++;
-        }
-    }
+	//Copy the reverse landmarks found into lamarr
+	for (int i = 0; i < lamarrRev.count; i++)
+	{
+		if (lamarr.count < 99)
+		{
 
-    for (int i=0; i<lamarr.count; i++) {
-        landm lam = lamarr.lm[i];
-        ROS_INFO("Landmark at %3d,%3d to %d,%d - code = %d\n",
-                 lam.xtop, lam.ytop, lam.xbottom, lam.ybottom, lam.code);
+			lamarr.lm[lamarr.count] = lamarrRev.lm[i];
 
-        landmarkSelfSim::landmarkLocation landmarkLoc;
-        landmarkLoc.header.stamp = ros::Time::now();
-        //Copy over the fields
-        landmarkLoc.xtop = lam.xtop;
-        landmarkLoc.ytop = lam.ytop;
-        landmarkLoc.xbottom = lam.xbottom;
-        landmarkLoc.ybottom = lam.ybottom;
-        landmarkLoc.code = lam.code;
-        //Compute the distance
-        landmarkLoc.height = sqrt((lam.xtop-lam.xbottom)*(lam.xtop-lam.xbottom)
-                                  +(lam.ytop-lam.ybottom)*(lam.ytop-lam.ybottom) );
-        //Publish it
-        landmark_pub_.publish(landmarkLoc);
+			int temp = lamarr.lm[lamarr.count].xbottom;
+			lamarr.lm[lamarr.count].xbottom = 320-lamarr.lm[lamarr.count].xtop;
+			lamarr.lm[lamarr.count].xtop = 320-temp;
 
-        //Now add the marks to the image to indicate the detected frame
+			temp = lamarr.lm[lamarr.count].ybottom;
+			lamarr.lm[lamarr.count].ybottom = 240-lamarr.lm[lamarr.count].ytop;
+			lamarr.lm[lamarr.count].ytop = 240-temp;
+
+			lamarr.count++;
+		}
+	}
+
+	for (int i=0; i<lamarr.count; i++) {
+		landm lam = lamarr.lm[i];
+		ROS_INFO("Landmark at %3d,%3d to %d,%d - code = %d\n",
+				 lam.xtop, lam.ytop, lam.xbottom, lam.ybottom, lam.code);
+		//ROS_INFO("rim1 width: %3d, height: %3d",rim1->width, rim1->height);
+		landmarkSelfSim::landmarkLocation landmarkLoc;
+		landmarkLoc.header.stamp = ros::Time::now();
+		//Copy over the fields
+		landmarkLoc.xtop = lam.xtop;
+		landmarkLoc.ytop = lam.ytop;
+		landmarkLoc.xbottom = lam.xbottom;
+		landmarkLoc.ybottom = lam.ybottom;
+		landmarkLoc.code = lam.code;
+		//Compute the distance
+		landmarkLoc.height = sqrt((lam.xtop-lam.xbottom)*(lam.xtop-lam.xbottom)
+								  +(lam.ytop-lam.ybottom)*(lam.ytop-lam.ybottom) );
+		//Publish it
+		landmark_pub_.publish(landmarkLoc);
+
+		//Now add the marks to the image to indicate the detected frame
 #define PLUS_SIZE 4
 #define PLUS_COLOR cv::Scalar(0,255,0,0)
-        cv::line(smlImg,
-                 cv::Point(lam.xtop*SCALE_FACTOR+PLUS_SIZE,lam.ytop*SCALE_FACTOR),
-                 cv::Point(lam.xtop*SCALE_FACTOR-PLUS_SIZE,lam.ytop*SCALE_FACTOR),
-                 PLUS_COLOR,1);
-        cv::line(smlImg,
-                 cv::Point(lam.xtop*SCALE_FACTOR,lam.ytop*SCALE_FACTOR+PLUS_SIZE),
-                 cv::Point(lam.xtop*SCALE_FACTOR,lam.ytop*SCALE_FACTOR-PLUS_SIZE),
-                 PLUS_COLOR,1);
-        cv::line(smlImg,
-                 cv::Point(lam.xbottom*SCALE_FACTOR+PLUS_SIZE,lam.ybottom*SCALE_FACTOR),
-                 cv::Point(lam.xbottom*SCALE_FACTOR-PLUS_SIZE,lam.ybottom*SCALE_FACTOR),
-                 PLUS_COLOR,1);
-        cv::line(smlImg,
-                 cv::Point(lam.xbottom*SCALE_FACTOR,lam.ybottom*SCALE_FACTOR+PLUS_SIZE),
-                 cv::Point(lam.xbottom*SCALE_FACTOR,lam.ybottom*SCALE_FACTOR-PLUS_SIZE),
-                 PLUS_COLOR,1);
-        stringstream ss;
-        ss << lam.code;
-        cv::putText(smlImg,ss.str(),
-                    cv::Point(lam.xbottom*SCALE_FACTOR-PLUS_SIZE*5,lam.ybottom*SCALE_FACTOR-PLUS_SIZE),
-                    cv::FONT_HERSHEY_SIMPLEX,SCALE_FACTOR,PLUS_COLOR);
-    }
+		cv::line(smlImg,
+				 cv::Point(lam.xtop*SCALE_FACTOR+PLUS_SIZE,lam.ytop*SCALE_FACTOR),
+				 cv::Point(lam.xtop*SCALE_FACTOR-PLUS_SIZE,lam.ytop*SCALE_FACTOR),
+				 PLUS_COLOR,1);
+		cv::line(smlImg,
+				 cv::Point(lam.xtop*SCALE_FACTOR,lam.ytop*SCALE_FACTOR+PLUS_SIZE),
+				 cv::Point(lam.xtop*SCALE_FACTOR,lam.ytop*SCALE_FACTOR-PLUS_SIZE),
+				 PLUS_COLOR,1);
+		cv::line(smlImg,
+				 cv::Point(lam.xbottom*SCALE_FACTOR+PLUS_SIZE,lam.ybottom*SCALE_FACTOR),
+				 cv::Point(lam.xbottom*SCALE_FACTOR-PLUS_SIZE,lam.ybottom*SCALE_FACTOR),
+				 PLUS_COLOR,1);
+		cv::line(smlImg,
+				 cv::Point(lam.xbottom*SCALE_FACTOR,lam.ybottom*SCALE_FACTOR+PLUS_SIZE),
+				 cv::Point(lam.xbottom*SCALE_FACTOR,lam.ybottom*SCALE_FACTOR-PLUS_SIZE),
+				 PLUS_COLOR,1);
+		stringstream ss;
+		ss << lam.code;
+		cv::putText(smlImg,ss.str(),
+					cv::Point(lam.xbottom*SCALE_FACTOR-PLUS_SIZE*5,lam.ybottom*SCALE_FACTOR-PLUS_SIZE),
+					cv::FONT_HERSHEY_SIMPLEX,SCALE_FACTOR,PLUS_COLOR);
+	}
 
-    //Send some images out
-    //cv_ptr->image = colorImg;
-    cv_ptr->image = smlImg;
-    cv_ptr->encoding = enc::BGR8;
-    outImg_pub_.publish(cv_ptr->toImageMsg());
-    //cv::resize(colorImgRev,smlImg,cv::Size(),SCALE_FACTOR,SCALE_FACTOR,
-    //           cv::INTER_NEAREST);
+	//Send some images out
+	//cv_ptr->image = colorImg;
+	cv_ptr->image = smlImg;
+	cv_ptr->encoding = enc::BGR8;
+	outImg_pub_.publish(cv_ptr->toImageMsg());
+	//cv::resize(colorImgRev,smlImg,cv::Size(),SCALE_FACTOR,SCALE_FACTOR,
+	//           cv::INTER_NEAREST);
    // cv_ptr->image = smlImg;
-   // outImg_pub_.publish(cv_ptr->toImageMsg());   
+   // outImg_pub_.publish(cv_ptr->toImageMsg());
 
 }
 
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "landmark_detector");
-    LandmarkDetector bd;
-    ros::spin();
-    return 0;
+	ros::init(argc, argv, "landmark_detector");
+	LandmarkDetector bd;
+	ros::spin();
+	return 0;
 }
